f06a562e545d8d09b99381925d51dfad164500da
Jatin Patware <36076932+jatincse19@users.noreply.github.com>
2018-04-12 15:26:02 +0530

    Add files via upload

---
+++ Greedy_and_dynamic/weak1_schedule1.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+#define n 10000
+using namespace std;
+
+int main() {
+
+	vector<int> weight;
+	vector<int> length;
+	vector<int> score;
+	ifstream infile;
+	infile.open("jobs.txt", ios::in);
+	string line;
+    int w,l;
+    //int n;
+    //infile>>n;
+	while (getline(infile, line)) {
+
+		infile>>w>>l;
+		//cout<<w<<" +++"<<l<<endl;
+		weight.push_back(w);
+		length.push_back(l);
+	}
+	infile.close();
+	for(int i=0;i<n;i++) {
+       score.push_back(weight[i]-length[i]);
+       //score.push_back(weight[i]/length[i]);
+	}
+    for(int i=0;i<n-1;i++) {
+        for(int j=0;j<n-i-1;j++) {
+            if(weight[j]<weight[j+1]) {
+                swap(weight[j],weight[j+1]);
+                swap(length[j],length[j+1]);
+                swap(score[j],score[j+1]);
+            }
+        }
+    }
+
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<n-i-1;j++) {
+            if(score[j]<score[j+1]) {
+                swap(score[j],score[j+1]);
+                swap(weight[j],weight[j+1]);
+                swap(length[j],length[j+1]);
+
+            }
+        }
+    }
+    long long int completion_time=length[0];
+    long long  int ans=0;
+    for(int i=0;i<n;i++) {
+        cout<<weight[i]<<" "<<length[i]<<" "<<completion_time<<" ";
+        ans+=completion_time*weight[i];
+        completion_time+=length[i+1];
+        cout<<ans<<endl;
+    }
+    cout<<ans<<endl;
+     //69119377652
+	return 0;
+}
+
+
+
+
+
+
+
